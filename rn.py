c29f2cfc7482ceff54c84da234622956e1ff1b62
TheShadow88 <stef4oben88@gmail.com>
2016-01-08 13:50:33 +0200

    Random list bigger number

---
+++ rn.py
@@ -0,0 +1,34 @@
+#-*-coding:utf8;-*-
+#qpy:2
+#qpy:console
+import random
+
+
+def biggest(lst):
+    c = 0
+    champ = 0
+    while c < len(lst):
+        if champ > lst[c]:
+            c += 1
+        else:
+            champ = lst[c]
+            c += 1
+    return champ
+     
+     
+c = 0
+ls = []
+while c < 6:
+    ls.append(random.randint(0, 20))
+    c += 1
+    
+print("The old list " + str(ls))
+c = 0
+nls = []
+k = len(ls)
+while c < k:
+    nls.append(biggest(ls))
+    ls.remove(biggest(ls))
+    c += 1
+print("The new list " + str(nls))
+print("The biggest number is " + str(nls[0]))
